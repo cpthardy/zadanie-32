967b08198e053d9230d1915f6c91833895918a2d
Maciej Wydrych <m.wydrych@gmail.com>
2018-04-16 22:41:33 +0200

    wykonanie zadania

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var num1 = 2;
+var num2 = 5;
+function biggerNumber(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+console.log(biggerNumber(num1, num2));
